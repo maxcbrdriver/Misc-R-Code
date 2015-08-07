ab9ca85fbc4d52f4198090b814af88e8e76d2494
maxcbrdriver <maxcbrdriver@gmail.com>
2015-08-07 17:04:25 -0400

    Added more ZEMA data file parsing.

--- 20150513 ZEMA v1.R
+++ 20150513 ZEMA v1.R
@@ -84,73 +84,171 @@ for(file.i in inp.files) {
 library(data.table)
 library(magrittr)
 library(stringi)
+library(ggplot2)
+
+as.IDate.f <- function(date.ch) {
+  as.IDate(date.ch, "%m/%d/%Y", tz='GMT')
+}
+
+# DST
+dst.dt <- fread("C:/Temp/DST Start End v2.csv", header=T)
+dst.dt[,Date.Dt:=as.IDate.f(Date)]
+dst.dt[,Date:=NULL]
+setnames(dst.dt, "Date.Dt", "Date")
+setcolorder(dst.dt, c("Date","DST Type"))
+setkey(dst.dt, "Date")
+
 
 setwd("C:/")
-base.dir <- "C:/Temp/ZEMA/20150618b/"
-sub.dirs <- c("GasFWD", "PowerFWD 2013 Batch 1", "PowerFWD 2014 Batch 1", "PwerFWD 2015 Batch 1", "PowerFWD NY Rerun")
-sub.dirs <- c("20150521", "CurrentForwardsCoalMonthly")
-sub.dirs <- c("CurrentPowerMonthlyExtra - 06.01.2015")
-sub.dirs <- c("Tenn New")
-sub.dirs <- c("CurrentForwardsOilMonthly - 05.27.2015")
-sub.dirs <- c("CurrentSpotCoalDaily - 06.05.2015 - Re-run", "CurrentSpotOilDaily - 05.27.2015")
-sub.dirs <- c("NE Mass Monthly", "NY Zone A Monthly", "NY Zone G & J Monthly", "PJM West Monthly")
+base.dir <- c("C:/Temp/ZEMA/20150714b/")
+#sub.dirs <- c("Power 02 Spot NE", "Power 02 Spot NY", "Power 02 Spot PJM")
+#sub.dirs <- c("20150618a/PJM West Monthly", "20150622a/PJM West Daily", "20150525a/20150521")
+sub.dirs <- "Power 02 Spot PJM/PJM_AECO"
 out.dir <- stri_paste(base.dir, "Upload/")
 
 full.dirs <- sapply(sub.dirs, . %>% stri_paste(base.dir, ., sep=""))
 files.dt <- lapply(full.dirs, function(x) data.table(File.Name=list.files(path=x, full.names=T, recursive = T))) %>% 
               rbindlist(use.names=T)
-files.dt[,File.Name.2:=stri_replace_all_regex(File.Name, "(CFGMT0\\d)_(\\d{2})", "$1$2")]
 
-# Forwards
-files.dt[, ":="(c("Cmmdty", "Spot", "Model", "Basis", "Loc", "QDate"), 
-                stri_match_all_regex(File.Name.2, 
-                                     "ForwardQuotes_([^_]*_){5}([^_]*)_([^_]*)_([^_]*_[^_]*_[^_]*)_([^_]*)_(.*?)_(\\d{2}-\\d{2}-\\d{4})")[[1]][3:8] %>% as.list),
-         by=File.Name.2]
+# Spot or Forward?
+files.dt[,Type:=stri_match_first_regex(File.Name, "([^/_]*)[^/]*$")[,2]]
+setkey(files.dt, Type)
+
+files.dt[,':='(c("Profile SC", "Provider SC", "TariffType SC", "RunType SC", "Cmmdty", "Spot", "Model", "Basis", "Loc", "QDate"), "")]
+
+# File Name Decomposition
+# Forward
+files.dt[J("ForwardQuotes"),
+         ':='(c("Profile SC", "Provider SC", "TariffType SC", "RunType SC", "Cmmdty", "Spot", "Model", "Basis", "Loc", "QDate"), 
+                stri_match_all_regex(File.Name, 
+                                     "ForwardQuotes_([^_]*)_([^_]*)_([^_]*)_([^_]*)__([^_]*)_([^_]*)_([^_]*_[^_]*_[^_]*)_([^_]*)_(.*?)_(\\d{2}-\\d{2}-\\d{4})")[[1]][2:11] %>% as.list),
+         by=File.Name]
 # Spot 
-files.dt[, ":="(c("Cmmdty", "Spot", "Model", "Basis", "Loc", "QDate"), 
-                stri_match_all_regex(File.Name.2, 
+files.dt[J("History"),
+         ':='(c("Cmmdty", "Spot", "Model", "Basis", "Loc", "QDate"), 
+                stri_match_all_regex(File.Name, 
                                      "History_Spot_([^_]*_){4}([^_]*)_([^_]*)_([^_]*_[^_]*_[^_]*)_([^_]*)_(.*?)_(\\d{2}-\\d{2}-\\d{4})")[[1]][3:8] %>% as.list),
-         by=File.Name.2]
+         by=File.Name]
 
 files.dt[,RF:=stri_paste(Cmmdty, Spot, Model, Basis, Loc, sep="_")]
 setkey(files.dt, RF)
 
+# Process Files
 # Forwards
-read.prc.f <- function(file.x) {
+read.frwd.prc.f <- function(file.x) {
   x.dt <- fread(file.x, header=T)
   setkey(x.dt, QuoteValue)
-  y.dt <- x.dt[!J(-999999)]
-  y.dt
+  x.dt <- x.dt[!J(-999999)]
+  x.dt
 }
 
-proc.file.grp.f <- function(file.grp, by.l) {
-  out.dt <- lapply(file.grp, read.prc.f) %>% rbindlist(use.names=T)
-  setorder(out.dt, QuoteDate, RiskFactor, StartDate, QuoteType)
-  write.csv(out.dt, stri_paste(out.dir,by.l$RF, ".csv",sep=""), quote = F, row.names = F)
+proc.frwd.file.grp.f <- function(file.grp, by.l) {
+  out.1.dt <- lapply(file.grp, read.frwd.prc.f) %>% rbindlist(use.names=T)
+  
+  out.1.dt %>% setkey(NULL)
+  out.2.dt <- unique(out.1.dt)
+  
+  out.2.dt[,':='(c("QuoteDate.Dt", "StartDate.Dt", "EndDate.Dt"), lapply(.SD, as.IDate.f)), .SDcols=c("QuoteDate", "StartDate", "EndDate")]
+  
+  out.2.dt[,Daily:=ifelse(StartDate.Dt==EndDate.Dt,T,F)]
+  out.2.dt[,Keep:=ifelse(EndDate.Dt>=QuoteDate.Dt,T,F) ]
+  
+  setkey(out.2.dt, Keep)
+  out.3.dt <- out.2.dt[J(T)]
+  setorder(out.3.dt, RiskFactor, QuoteDate.Dt, StartDate.Dt, QuoteType)
+  out.3.dt[,':='(QuoteDate.Dt=NULL, StartDate.Dt=NULL, EndDate.Dt=NULL, Daily=NULL, Keep=NULL)]
+  write.csv(out.3.dt, stri_paste(out.dir,"ForwardQuotes_", by.l$RF, ".csv",sep=""), quote = F, row.names = F)
+  
   1
 }
 
-tmp.dt <- files.dt[,proc.file.grp.f(File.Name, .BY), by=RF]
-
 # Spot
-read.spot.f <- function(file.x) {
-  print(file.x)
+read.spot.prc.f <- function(file.x) {
   x.dt <- fread(file.x, header=T)
   setkey(x.dt, `1`)
   y.dt <- x.dt[!J(-999999)]
   y.dt
 }
 
-proc.spot.grp.f <- function(file.grp, by.l) {
-  in.dt <- lapply(file.grp, read.spot.f) %>% rbindlist(use.names=T)
-  out.dt <- in.dt[,list(RiskFactor=unique(RiskFactor), `1`=mean(`1`)), by=Date]
-  setorder(out.dt, Date)
-  setcolorder(out.dt, c("Date", "RiskFactor", "1"))
-  write.csv(out.dt, stri_paste(out.dir, by.l$RF, ".csv", sep=""), quote=F, row.names=F)
-  1
+proc.spot.file.grp.f <- function(file.grp, by.l) {
+  in.dt <- lapply(file.grp, read.spot.prc.f) %>% rbindlist(use.names=T)
+  
+  in.dt %>% setkey(NULL)
+  out.1.dt <- unique(in.dt)
+  
+  setkey(out.1.dt, Date)
+  max.cnt <- out.1.dt[,.N, by=Date][,max(N)]
+  
+  out.2.dt <- out.1.dt[,list(RiskFactor=unique(RiskFactor), `1`=mean(`1`)), by=Date]
+  out.2.dt[,Date.Dt:=as.IDate.f(Date)]
+  setorder(out.2.dt, Date.Dt)
+  out.2.dt[,Date.Dt:=NULL]
+  setcolorder(out.2.dt, c("Date", "RiskFactor", "1"))
+  write.csv(out.2.dt, stri_paste(out.dir, "History_Spot_", by.l$RF, ".csv", sep=""), quote=F, row.names=F)
+  
+  max.cnt
 }
 
-tmp.dt <- files.dt[,proc.spot.grp.f(File.Name, .BY), by=RF]
+read.spot.pwr.2.prc.f <- function(file.x) {
+  require(data.table)
+  require(magrittr)
+  
+  # Import
+  a.dt <- fread(file.x, header=F, skip=1)
+  
+  # Assign Column Names
+  setnames(a.dt, c("Date", "RiskFactor", 1:(length(a.dt)-2)))
+  a.dt[,':='(seq(4, length(a.dt), 2), NULL)]
+  setnames(a.dt, c("Date", "RiskFactor", 1:(length(a.dt)-2)))
+  
+  a.dt[,Date.Dt:=as.IDate.f(Date)]
+  a.dt[,Date:=NULL]
+  setnames(a.dt, "Date.Dt", "Date")
+  setcolorder(a.dt, c(length(a.dt), 1:(length(a.dt)-1)))
+  
+  setkey(a.dt, Date)
+  
+  b.dt <- dst.dt[a.dt, nomatch=NA]
+  setkey(b.dt, "DST Type")
+  b.dt[J("Spring"), `3`:= mean(`2`, `4`), nomatch=0]
+  b.dt[J("Fall"), `3`:= mean(`3`, `4`), nomatch=0]
+  x <- b.dt[J("Fall"), 8:28, with=F] %>% as.numeric
+  b.dt[J("Fall"), ':='(7:27, as.list(x))]
+  b.dt[,`25`:=NULL]
+  
+  
+  
+  b.dt <- melt.data.table(a.dt, id.vars=c("Date", "RiskFactor"), variable.name="HE", value.name="Price", variable.factor=F)
+  b.dt[,':='(HE.num=(as.numeric(HE)+1)/2, Date.Dt=as.IDate.f(Date))]
+  b.dt[,HE:=NULL]
+  setnames(b.dt, "HE.num", "HE")
+  
+  b.dt %>% setkey(HE)
+  c.dt <- b.dt[J(1:24)]
+  c.dt %>% setorder(RiskFactor, Date.Dt, HE)
+  
+  c.dt %>% setkey(Price)
+  d.dt <- c.dt[!J(-999)]
+  
+  
+  setkey(x.dt, `1`)
+  y.dt <- x.dt[!J(-999999)]
+  y.dt
+}
+
+proc.spot.pwr.2.fle.grp.f <- function(file.grp, by.l) {
+  in.dt <- lapply(file.grp, read)
+  
+}
+
+
+files.dt %>% setkey("Type")
+frwd.dt <- files.dt[J("ForwardQuotes"),proc.frwd.file.grp.f(File.Name, .BY), by=RF]
+spot.dt <- files.dt[J("History"),proc.spot.file.grp.f(File.Name, .BY), by=RF]
+
+
+spot.dt <- files.dt[J("History"), proc.spot.file.grp.2.f(File.Name, QDate, .BY), by=RF]
+
 
 
 # Part 3 - Review
@@ -208,4 +306,27 @@ read.Gas.f <- function(file.str) {
 Gas.dt <- lapply(files.Gas, read.Gas.f) %>% rbindlist()
 Gas.2.dt <- dcast(Gas.dt, AsOf.IDt ~ Prc.IDt + RiskFactor, value.var = "Prc")
 
+# Part 5
+setwd("C:/Temp/ZEMA/20150629a/Upload/")
+a.dt <- fread("Gas_GD_MF__L_LD1_Hub_HH.csv", header=T)
+a.dt[,':='(QuoteDate.Dt=as.IDate.f(QuoteDate), StartDate.Dt=as.IDate.f(StartDate), EndDate.Dt=as.IDate.f(EndDate))]
+a.dt[,Daily:=ifelse(StartDate.Dt==EndDate.Dt, T, F)]
+a.dt[,range(QuoteDate.Dt), by=Daily]
+
+a <- ggplot(a.dt[,.N, by=c("QuoteDate.Dt", "Daily")], aes(x=QuoteDate.Dt, y=N, color=Daily)) + geom_point()
+a
+setkey(a.dt, Daily)
+b <- ggplot(a.dt[J(FALSE),.N, by="QuoteDate.Dt"], aes(x=QuoteDate.Dt, y=N)) + geom_line()
+b
+
+setkey(a.dt, QuoteDate.Dt, Daily, StartDate.Dt)
+a.dt[J(as.IDate.f("1/25/2010"),F)]
+a.dt[J(as.IDate.f("8/1/2011"),F)]
+a.dt[J(as.IDate.f("8/1/2012"),F)]
+a.dt[J(as.IDate.f("6/10/2015"),F)]
+
+a.dt[J(as.IDate.f("1/25/2010"),T)]
+a.dt[J(as.IDate.f("8/1/2011"),T)]
+a.dt[J(as.IDate.f("8/1/2012"),T)]
+a.dt[J(as.IDate.f("6/10/2015"),T)]
 
