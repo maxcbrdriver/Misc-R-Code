e816d6f2ee5c11ecbdf26f384d4e9d27d061a97b
maxcbrdriver <maxcbrdriver@gmail.com>
2015-12-18 13:17:54 -0500

    Misc testing

--- 20140505 Test Setup v1.R
+++ 20140505 Test Setup v1.R
@@ -10,18 +10,16 @@ cat("\014")   # Same as 'Ctrl-L', clears Console
 
 # ----- Library -----
 # Default library specified in Windows Environment variable R_LIBS_USER
-.libPaths() #
-#.libPaths("C://R/Libraries//RRO_3.2.2")  # Default
-#.libPaths("C://R/Libraries//RRO_3.2.1")  #
-#.libPaths("C://R/Libraries//RRO_3.2.0")  #
-#.libPaths("C://R/Libraries//R_3.2.0")    #
-#.libPaths("C://R/Libraries//R_2.15.3")   #
+.libPaths()
+#.libPaths("C://R/Libraries//RRO-3.2.2")  # Default
+#.libPaths("C://R/Libraries//R-3.2.2")   #
+#.libPaths("C://R/Libraries//R-2.15.3")   #
 .Library
 .Library.site
 
 # ----- Rprofile -----
 # Location: etc/Rprofile.site
-# Removed the RRO default repository (located in etc\Rprofile.site) because it utilizes a specific snapshot
+# Removed the RRO default repository (located in etc\Rprofile.site) because it utilizes a specific snapshot 
 # date and therefore prevents updates to the latest version
 getOption('repos')
 
@@ -29,7 +27,7 @@ getOption('repos')
 # Github Development Packages
 # For some reason, the downloads from github produce the following SSL Certificate error. To ignore
 # this warning, setup `httr` to pass to `RCurl` a flag to ignore verification.
-#   Error in function (type, msg, asError = TRUE)  :
+#   Error in function (type, msg, asError = TRUE)  : 
 #   SSL certificate problem, verify that the CA cert is OK. Details:
 #   error:14090086:SSL routines:SSL3_GET_SERVER_CERTIFICATE:certificate verify failed
 library(RCurl)
@@ -39,8 +37,6 @@ set_config( config( ssl_verifypeer = 0L ) )   # Ignores the SSL check; Was once 
 #https://github.com/hadley/devtools
 #devtools::install_github("hadley/devtools")
 library(devtools)
-
-
 
 # Data.Table
 # https://github.com/Rdatatable/data.table/
@@ -191,7 +187,7 @@ library(microbenchmark)
 
 # GenomicRanges
 # Run R As Administrator ...
-source("https://bioconductor.org/biocLite.R")
+source("http://bioconductor.org/biocLite.R")
 biocLite("GenomicRanges")
 library(GenomicRanges)
 
@@ -266,7 +262,7 @@ print(p.all)
 
 setkey(All.Hrly.dt, Year, Month, PKPRD_2)
 res.1 <- microbenchmark(t.1 <- All.Hrly.dt[,mean(`LMP.DA.NY-K`, na.rm = T), by=list(Year, Month, PKPRD_2)], times=1000L)
-res.2 <- microbenchmark(t.2 <- All.Hrly.dt %>%
+res.2 <- microbenchmark(t.2 <- All.Hrly.dt %>% 
           select(`LMP.DA.NY-K`, Year, Month, PKPRD_2) %>%
           group_by(Year, Month, PKPRD_2) %>%
           summarize(V1=mean(`LMP.DA.NY-K`, na.rm=T)),
@@ -309,8 +305,8 @@ dbListTables(my_db)
 t.1.dt <- data.table(a=1:10, b=LETTERS[1:10], c=ISOdate(2014, 10, 11:20, 0, tz='GMT'))
 str(t.1.dt)
 dbWriteTable(my_db, 't_1', t.1.dt)
-t.2.dt <- data.table(a=round(runif(1e6)*100,0),
-                     b=LETTERS[sample(1:26, 1e6, T)],
+t.2.dt <- data.table(a=round(runif(1e6)*100,0), 
+                     b=LETTERS[sample(1:26, 1e6, T)], 
                      c=ISOdate(2014, sample(1:12, 1e6, T), sample(1:28, 1e6, T), 0, tz='GMT'))
 setkey(t.2.dt, c)
 t.2.dt[,d:=strftime(c, "%Y-%m-%d"), by=c]
@@ -423,25 +419,25 @@ library(plotly)
 py <- plotly(username='R-Demo-Account', key='yu680v5eii')
 
 trace1 <- list(
-  x = c(1, 2, 3),
-  y = c(4, 3, 2),
+  x = c(1, 2, 3), 
+  y = c(4, 3, 2), 
   type = "scatter"
 )
 trace2 <- list(
-  x = c(20, 30, 40),
-  y = c(30, 40, 50),
-  xaxis = "x2",
-  yaxis = "y2",
+  x = c(20, 30, 40), 
+  y = c(30, 40, 50), 
+  xaxis = "x2", 
+  yaxis = "y2", 
   type = "scatter"
 )
 data <- list(trace1, trace2)
 layout <- list(
   yaxis2 = list(
-    domain = c(0.6, 0.95),
+    domain = c(0.6, 0.95), 
     anchor = "x2"
-  ),
+  ), 
   xaxis2 = list(
-    domain = c(0.6, 0.95),
+    domain = c(0.6, 0.95), 
     anchor = "y2"
   )
 )
@@ -454,7 +450,7 @@ library(R6)
 env.tree.f <- function(x) {
   par.env <- parent.env(x)
   print(attr(par.env, "name"))
-
+  
   if(!identical(par.env, emptyenv())) {
     env.tree.f(par.env)
   }
@@ -477,7 +473,7 @@ lf.dt[,`:=`(c('Run_Type', 'ProfileNameSC', 'ProviderSC', 'TariffSC', 'RunTypeSC'
             as.list(stri_split_regex(File_Name, "_|\\.")[[1]][c(1,2,3,4,5,7,8,9,10,11,12)])), by=File_Name]
 lf.dt[,`:=`('AsOf', as.IDate(stri_match_all_regex(File_Name, "(\\d{2}-\\d{2}-\\d{4})\\.csv$")[[1]][2], "%m-%d-%Y", tz='GMT')), by=File_Name]
 lf.dt %>% str
-
+      
 
 # Test GBM
 # Illustrates that the price level itself directly affects the GMaR (in this case using a single unit long position)
@@ -502,15 +498,16 @@ a <- t.3.dt %>% ggplot(aes(x=`Prc@t0`, y=GMaR)) + geom_point() + geom_line(); a
 b <- t.3.dt %>% ggplot(aes(x=`Prc@t0`, y=`Chg%`)) + geom_point() + geom_line() + coord_cartesian(ylim=c(-0.9, 0)); b
 c <- t.3.dt %>% ggplot(aes(x=`Prc@t0`, y=`ChgLn%`)) + geom_point() + geom_line() + coord_cartesian(ylim=c(-1, 0)); c
 
-
-
+# Lacima Profile Testing
+.libPaths("C://R/Libraries//RRO_3.2.2")
 library(data.table)
 library(magrittr)
-setwd("C:/Temp")
+library(ggplot2)
+setwd("C:/Temp/Lacima")
 
 as.IDate.mdY.1.f <- . %>% as.IDate(., "%m/%d/%Y", tz="America/New_York")
 
-prof.dt <- fread("20151112 MF Profile v1.csv", header=T)
+prof.dt <- fread("20151123 PJM West v1.csv", header=T)
 
 prof.2.dt <- melt(prof.dt, id.vars = "Date", variable.name = "HE.Chr", variable.factor = F, value.name = "Prc")
 prof.2.dt[,HE:=as.numeric(HE.Chr)]
@@ -524,14 +521,196 @@ prof.2.dt[CJ(WkDay=c(1:5), HE=c(8:23)),Pk:=1, on=c("WkDay", "HE")]
 setorder(prof.2.dt, Date.IDt, HE)
 prof.2.dt %>% head(24)
 
+# Setup
 all.pk.avg.dt <- prof.2.dt[,list("All.Pk.Avg"=mean(Prc)), by=Pk]
-seas.pk.avg.dt <- prof.2.dt[,list("Seas.Pk.Avg"=mean(Prc)), by=list(Mnth, Pk)]
-wkday.seas.pk.avg.dt <- prof.2.dt[,list("Wkday.Seas.Pk.Avg"=mean(Prc)), by=list(WkDay, Mnth, Pk)]
+seas.pk.avg.dt <- prof.2.dt[,list("Seas.Pk.Avg"=mean(Prc)), keyby=list(Mnth, Pk)]
+dly.seas.pk.avg.dt <- prof.2.dt[,list("Dly.Seas.Pk.Avg"=mean(Prc)), by=list(Mnth, WkDay, Pk)]
+prd.dly.seas.pk.avg.dt <- prof.2.dt[,list("Prd.Dly.Seas.Pk.Avg"=mean(Prc)), by=list(Mnth, WkDay, HE, Pk)]
 
+# Seasonal Calc
 seas.pk.ratio.dt <- all.pk.avg.dt[seas.pk.avg.dt, on="Pk"]
 seas.pk.ratio.dt[,Ratio:=Seas.Pk.Avg/All.Pk.Avg]
+seas.pk.ratio.dt[,Adj:=mean(Ratio),by=Pk]
+seas.pk.ratio.dt[,Adj.Ratio:=Ratio/Adj]
+seas.pk.ratio.dt[,mean(Adj.Ratio),by=All.Pk.Avg]
+dcast(seas.pk.ratio.dt, Mnth ~ Pk, value.var="Adj.Ratio", fun.aggregate=list(mean, sd))
+
+# Weekday Calc
+dly.seas.pk.ratio.dt <- dly.seas.pk.avg.dt[seas.pk.avg.dt, on=c("Mnth", "Pk")]
+dly.seas.pk.ratio.dt[,Ratio:=Dly.Seas.Pk.Avg/Seas.Pk.Avg]
+dly.seas.pk.ratio.dt[,Adj:=mean(Ratio), by=c("Mnth", "Pk")]
+dly.seas.pk.ratio.dt[,Adj.Ratio:=Ratio/Adj]
+dly.seas.pk.ratio.dt[,mean(Adj.Ratio), by=c("Mnth", "Pk")]
+dcast(dly.seas.pk.ratio.dt, Mnth+WkDay ~ Pk, value.var = "Adj.Ratio", fun.aggregate = list(mean, sd))
+
+# Period Calc
+prd.dly.seas.pk.ratio.dt <- prd.dly.seas.pk.avg.dt[dly.seas.pk.avg.dt, on=c("Mnth", "WkDay", "Pk")]
+prd.dly.seas.pk.ratio.dt[,Ratio:=Prd.Dly.Seas.Pk.Avg/Dly.Seas.Pk.Avg]
+dcast(prd.dly.seas.pk.ratio.dt, Mnth+WkDay+Pk ~ HE, value.var = "Ratio", fun.aggregate = list(mean, sd))
+
+# Seasonal * Weekday
+seas.pk.ratio.dt[dly.seas.pk.ratio.dt, on=c("Mnth", "Pk")]
+
+# Transco Analysis
+library(data.table)
+library(magrittr)
+library(ggplot2)
+library(XLConnect)
+setwd("C:/Temp/Lacima")
+
+as.IDate.mdY.1.f <- . %>% as.IDate(., "%m/%d/%Y", tz="America/New_York")
+
+Tr.Prcs.1.dt <- readWorksheetFromFile("20151207 Transco Spot Pricing v1.xls",sheet="Sheet1")
+Tr.Prcs.1.dt <- as.data.table(Tr.Prcs.1.dt)
+
+Tr.Prcs.1.dt[,Date.IDt:=as.IDate.mdY.1.f(Date)]
+Tr.Prcs.1.dt[,Date:=NULL]
+
+Tr.Prcs.2.dt <- melt(Tr.Prcs.1.dt, id.vars=c("Date.IDt"), variable.name="Loc", variable.factor = F, value.name = "Prc", value.factor = F)
+
+setkey(Tr.Prcs.2.dt, Prc)
+Tr.Prcs.3.dt <- Tr.Prcs.2.dt[!J(-999999), on="Prc"]
+Tr.Prcs.4.dt <- dcast.data.table(Tr.Prcs.3.dt, Date.IDt ~ Loc, value.var = "Prc")
+
+setkey(Tr.Prcs.3.dt, Loc)
 
 
-wkday.seas.pk.ratio.dt <- all.pk.avg.dt[wkday.seas.pk.avg.dt, on="Pk"]
-wkday.seas.pk.ratio.dt[,Ratio:=Wkday.Seas.Pk.Avg/All.Pk.Avg]
-setorder(wkday.seas.pk.ratio.dt, Mnth, WkDay, Pk)
+a <- ggplot(Tr.Prcs.3.dt[J(c("Transco_Zn_1", "Transco_Zn_2"))], aes(x=Date.IDt, y=Prc, color=Loc)) + geom_line() + geom_point()
+a
+
+b <- ggplot(Tr.Prcs.4.dt, aes(x=Transco_Zn_1, y=Transco_Zn_2)) + geom_line() + geom_point()
+b
+
+b.reg <- lm(data=Tr.Prcs.4.dt[,list(Transco_Zn_1, Transco_Zn_2)], Transco_Zn_2 ~ Transco_Zn_1)
+b.reg %>% summary
+
+
+c <- ggplot(Tr.Prcs.4.dt, aes(x=Transco_Zn_3, y=Transco_Zn_2)) + geom_line() + geom_point()
+c
+
+c.reg <- lm(data=Tr.Prcs.4.dt[,list(Transco_Zn_3, Transco_Zn_2)], Transco_Zn_2 ~ Transco_Zn_3)
+c.reg %>% summary
+
+Tr.Prcs.4.dt[,Mnth:=month(Date.IDt)]
+Tr.Prcs.4.dt[,lm(Transco_Zn_2 ~ Transco_Zn_3, .SD) %>% summary.lm %>% `$`("r.squared"), by=Mnth]
+Tr.Prcs.4.dt[,lm(Transco_Zn_2 ~ Transco_Zn_1, .SD) %>% summary.lm %>% `$`("r.squared"), by=Mnth]
+
+r.sqrd.1.dt <- Tr.Prcs.4.dt[,list(Zn_1=lm(Transco_Zn_2 ~ Transco_Zn_1, .SD) %>% summary.lm %>% `$`("r.squared"),
+                                  Zn_3=lm(Transco_Zn_2 ~ Transco_Zn_3, .SD) %>% summary.lm %>% `$`("r.squared")),
+                            by=Mnth]
+r.sqrd.2.dt <- melt(r.sqrd.1.dt, id.vars="Mnth", variable.name="Ref_Zone", variable.factor = F, value.name = "R_Sqrd", value.factor = F )
+d <- ggplot(r.sqrd.2.dt, aes(x=Mnth, y=R_Sqrd, color=Ref_Zone)) + geom_line() + geom_point()
+d
+
+
+# Oil2 Analysis
+library(data.table)
+library(magrittr)
+library(ggplot2)
+library(XLConnect)
+setwd("C:/Temp/Lacima")
+
+as.IDate.mdY.1.f <- . %>% as.IDate(., "%m/%d/%Y", tz="America/New_York")
+
+Oil2.Prcs.1.dt <- readWorksheetFromFile("20151207 Oil2 Spot Pricing v1.xls",sheet="Sheet1")
+Oil2.Prcs.1.dt <- as.data.table(Oil2.Prcs.1.dt)
+
+Oil2.Prcs.1.dt[,Date.IDt:=as.IDate.mdY.1.f(Date)]
+Oil2.Prcs.1.dt[,Date:=NULL]
+
+Oil2.Prcs.2.dt <- melt(Oil2.Prcs.1.dt, id.vars=c("Date.IDt"), variable.name="Loc", variable.factor = F, value.name = "Prc", value.factor = F)
+
+setkey(Oil2.Prcs.2.dt, Prc)
+Oil2.Prcs.3.dt <- Oil2.Prcs.2.dt[!J(-999999), on="Prc"]
+Oil2.Prcs.3.dt[,`:=`(Yr=year(Date.IDt), Mnth=month(Date.IDt))]
+
+Oil2.Prcs.3.dt[,transpose(list(range(Prc))), by=Loc]
+Oil2.Prcs.3.dt[,list(Mean=mean(Prc), SD=sd(Prc)), by=Loc]
+
+Oil2.Prcs.4.dt <- dcast(Oil2.Prcs.3.dt, Date.IDt + Yr + Mnth ~ Loc, value.var = "Prc")
+
+
+a <- ggplot(Oil2.Prcs.3.dt[!J(c("ULSD")),on="Loc"][J(2015),on="Yr"], aes(x=Prc, color=Loc)) + geom_density()
+a
+
+b <- ggplot(Oil2.Prcs.3.dt[!J(c("ULSD")),on="Loc"][J(2010:2015),on="Yr"], aes(Prc, color=Loc)) + stat_ecdf()
+b
+
+c <- ggplot(Oil2.Prcs.4.dt, aes(x=ULSD, y=Jet_Kero_Basis, color=as.factor(Yr))) + geom_point()
+c
+
+
+b <- ggplot(Oil2.Prcs.4.dt, aes(sample=Jet_Kero_Basis)) + stat_qq()
+b
+
+c <- ggplot(data.table(X=rnorm(n=1000)), aes(sample=X)) + stat_qq()
+c
+
+norm.dt <- data.table(X=rnorm(n=1000000))
+norm.dt[,sum(ifelse(X<=-1,1,0))/.N]
+
+Oil2.Prcs.5.dt <- copy(Oil2.Prcs.3.dt)
+Oil2.Prcs.5.dt[,Ln.Prc:=0]
+Oil2.Prcs.5.dt[!J("ULSD"),Ln.Prc:=log(Prc+1), on="Loc"]
+
+a <- ggplot(Oil2.Prcs.5.dt[!J("ULSD"),on="Loc"][J(2015),on="Yr"], aes(x=Ln.Prc, color=Loc)) + geom_density()
+a
+
+
+library(XLConnect)
+library(data.table)
+library(magrittr)
+library(ggplot2)
+library(lubridate)
+library(fasttime)
+library(stringi)
+
+setwd("C:/Temp/Lacima")
+ps.prcs.df <- readWorksheetFromFile("20151214 PS Hist Basis v1.xlsx", sheet="Sheet1")
+ps.prcs.dt <- data.table(ps.prcs.df)
+
+setnames(ps.prcs.dt, c("Date", 1:24))
+
+ps.prcs.2.dt <- melt(ps.prcs.dt, id.vars="Date", variable.name="HE.Ch", variable.factor = FALSE, value.name = "Prc", value.factor = F)
+ps.prcs.2.dt[J(-999999),list(Date,HE.Ch), on="Prc", nomatch=0]
+ps.prcs.2.dt[J(-999999),Prc:=NA, on="Prc", nomatch=0]
+
+ps.prcs.2.dt[,HE:=as.numeric(HE.Ch)]
+ps.prcs.2.dt[,Date.Tm.Ch:= stri_paste(Date, HE.Ch, sep=" ")]
+ps.prcs.2.dt[,Date.Tm.Pct:=parse_date_time(Date.Tm.Ch, "%Y-%m-%d %H")]
+
+ps.prcs.3.dt <- ps.prcs.2.dt[Date.IDt>= as.IDate("6/1/2010", "%m/%d/%Y", tz="America/New_York") &
+                               Date.IDt<= as.IDate("5/31/2015", "%m/%d/%Y", tz="America/New_York")]
+
+ps.prcs.3.dt[,range(Prc)]
+
+ps.prcs.dist.dt <- ps.prcs.3.dt[,list(Prc)]
+setorder(ps.prcs.dist.dt, "Prc")
+ps.prcs.dist.dt[,Prct:=(1:.N)/.N]
+ps.prcs.dist.dt[J(c(0.0025, 0.005, 0.01, 0.05, 0.95, 0.99, 0.995, 0.9975)), Prc, on="Prct", roll=T, by=.EACHI]
+
+a <- ggplot(ps.prcs.3.dt, aes(x=Prc)) + stat_density() + coord_cartesian(xlim=c(-18, 56));a
+b <- ggplot(ps.prcs.3.dt, aes(x=log(Prc+31))) + stat_density();b
+
+qrtly.seas.cat.dt <- data.table(Date=c("1/1/2015", "3/15/2015", "6/15/2015", "9/15/2015", "12/15/2015"), 
+                                Season=c("Winter", "Spring", "Summer", "Fall", "Winter"))
+qrtly.seas.cat.dt[,Date.IDt:=as.IDate(Date, "%m/%d/%Y", tz="America/New_York")]
+qrtly.seas.cat.dt[,':='(c("Mnth", "Day"), list(month(Date.IDt), mday(Date.IDt)))]
+qrtly.seas.cat.dt[,':='(Date=NULL, Date.IDt=NULL)]
+
+ps.prcs.4.dt <- copy(ps.prcs.3.dt)
+ps.prcs.4.dt[,':='(c("Mnth", "Day"), list(month(Date.IDt), mday(Date.IDt)))]
+
+setkey(qrtly.seas.cat.dt, Mnth, Day)
+setkey(ps.prcs.4.dt, Mnth, Day)
+ps.prcs.5.dt <- qrtly.seas.cat.dt[ps.prcs.4.dt, roll=T]
+
+pk.dt <- data.table(HE=c(1,8,24), Pk=c("Off", "On", "Off"))
+
+setkey(ps.prcs.5.dt, HE)
+setkey(pk.dt, HE)
+ps.prcs.6.dt <- pk.dt[ps.prcs.5.dt, roll=T]
+
+setkey(ps.prcs.6.dt, Date.IDt, HE)
+ps.prcs.7.dt <- ps.prcs.6.dt[,by=list(Season, Pk)]
+
